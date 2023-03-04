db9d171136b1f602ec292c17b0b2b6bfb4009e4c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-03-04 08:05:22 +0600

    Complete all card design from API

--- scripts/universe-hub.js
+++ scripts/universe-hub.js
@@ -11,15 +11,31 @@ const displayAllCards = data => {
     const cardContainer = document.getElementById('card-container');
 
     data.tools.slice(0, 6).forEach(singleCard => {
-        // console.log(singleCard);
+        console.log(singleCard);
         const cardDiv = document.createElement('div');
-        cardDiv.classList.add('card');
+        cardDiv.classList.add('card', 'border-0');
         cardDiv.innerHTML = `
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${singleCard.image}" class="card-img-top h-100" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title fw-bold">Features</h5>
+                <div style="line-height: 10px;" class="mt-4">
+                    <p>1. <span>${singleCard.features[0]}</span></p>
+                    <p>2. <span>${singleCard.features[1]}</span></p>
+                    <p>3. <span>${singleCard.features[2]}</span></p>
+                </div>
+                <hr>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title fw-bold">${singleCard.name}</h5>
+                        <div class="d-flex gap-2 align-items-center ">
+                            <p><i class="fa-regular fa-calendar-days"></i></p>
+                            <p>${singleCard.published_in}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p><i class="fa-solid fa-arrow-right"></i></p>
+                    </div>
+                </div>
             </div>
         `
         cardContainer.appendChild(cardDiv);
