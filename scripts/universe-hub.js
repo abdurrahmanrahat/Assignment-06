b83258c498c4f939bd5ce18f64ee99c9392e6197
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-03-04 16:27:49 +0600

    complete display all btn by clicking see all button

--- scripts/universe-hub.js
+++ scripts/universe-hub.js
@@ -1,8 +1,8 @@
 // fetch card link
 const fetchCard = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(res => res.json())
-    .then(data => displayAllCards(data.data))
+        .then(res => res.json())
+        .then(data => displayAllCards(data.data))
 }
 
 // display all cards on UI 
@@ -10,7 +10,9 @@ const displayAllCards = data => {
     // console.log(data);
     const cardContainer = document.getElementById('card-container');
 
-    data.tools.slice(0, 6).forEach(singleCard => {
+    const card = data.tools.slice(0, 6);
+
+    card.forEach(singleCard => {
         console.log(singleCard);
         loadingSpinner(true);
         const cardDiv = document.createElement('div');
@@ -50,10 +52,10 @@ fetchCard();
 // spinner section
 const loadingSpinner = isLoading => {
     const spinnerDiv = document.getElementById('spinner');
-    if(isLoading){
+    if (isLoading) {
         spinnerDiv.classList.remove('d-none');
     }
-    else{
+    else {
         spinnerDiv.classList.add('d-none');
     }
 }
@@ -64,8 +66,8 @@ const fetchModalDetails = cardId => {
     // console.log(cardId);
     const url = `https://openapi.programming-hero.com/api/ai/tool/${cardId}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayModalDetails(data.data))    
+        .then(res => res.json())
+        .then(data => displayModalDetails(data.data))
 }
 
 const displayModalDetails = data => {
@@ -117,4 +119,59 @@ const displayModalDetails = data => {
             <p>${data.input_output_examples[0].output}</p>
         </div>
     `
+}
+
+
+// show all cards by clicking see more button
+const allCards = async () => {
+    // const allCardsurl = 'https://openapi.programming-hero.com/api/ai/tools';
+    // const res = await fetch(allCardsurl);
+    // const data = await res.json();
+    // displayAllCardsByBtn(data.data);
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res => res.json())
+    .then(data => displayAllCardsByBtn(data.data))
+}
+
+const displayAllCardsByBtn = data => {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    const card = data.tools;
+
+    card.forEach(singleCard => {
+        console.log(singleCard);
+        loadingSpinner(true);
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card', 'border-0');
+        cardDiv.innerHTML = `
+            <img src="${singleCard.image}" class="card-img-top h-100" alt="...">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">Features</h5>
+                <div style="line-height: 10px;" class="mt-4">
+                    <p>1. <span>${singleCard.features[0]}</span></p>
+                    <p>2. <span>${singleCard.features[1]}</span></p>
+                    <p>3. <span>${singleCard.features[2]}</span></p>
+                </div>
+                <hr>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title fw-bold">${singleCard.name}</h5>
+                        <div class="d-flex gap-2 align-items-center ">
+                            <p><i class="fa-regular fa-calendar-days"></i></p>
+                            <p>${singleCard.published_in}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p><i class="fa-solid fa-arrow-right" onclick="fetchModalDetails('${singleCard.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></p>
+                    </div>
+                </div>
+            </div>
+        `
+        cardContainer.appendChild(cardDiv);
+        loadingSpinner(false);
+    });
+
+    const seeAllBtn = document.getElementById('see-more-btn');
+    seeAllBtn.classList.add('d-none');
 }
